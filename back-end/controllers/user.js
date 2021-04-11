39ca93bb326b593493c8a923638786fda69a323f
maxencemachu <maxence.machu@akarah.com>
2021-04-11 16:23:16 +0200

    user: login & register

--- back-end/controllers/user.js
+++ back-end/controllers/user.js
@@ -1,9 +1,38 @@
 const sUser = require('../models/user');
+const bcrypt = require('bcrypt')
 
-exports.register = (req, res, next) => {
-    res.status(200).json({route : 'register'})
+exports.register = async (req, res, next) => {
+    
+    let hash = await bcrypt.hash(req.body.password, 10);
+    
+    const user = new sUser({
+        name: req.body.name,
+        email: req.body.email,
+        password: hash
+    });
+
+    user.save()
+    .then(() => res.status(201).json({ message: 'Crée !' }))
+    .catch(error => res.status(400).json({ error }));
 };
 
 exports.login = (req, res, next) => {
-    res.status(200).json({route : 'login'})
+    sUser.findOne({ email: req.body.email })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ error: 'Utilisateur introuvable' });
+      }
+      bcrypt.compare(req.body.password, user.password)
+        .then(valid => {
+          if (!valid) {
+            return res.status(401).json({ error: 'Mot de passe incorrect' });
+          }
+          res.status(200).json({
+            userId: user._id,
+            token: 'FUTUR_TOKEN'
+          });
+        })
+        .catch(error => res.status(500).json({ error }));
+    })
+    .catch(error => res.status(500).json({ error }));
 };
