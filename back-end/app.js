c102f4163194e3477024f256245ec0dc82f91e49
maxencemachu <maxence.machu@akarah.com>
2021-04-11 11:34:39 +0200

    add : mongo connection string

--- back-end/app.js
+++ back-end/app.js
@@ -1,7 +1,17 @@
+require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URL}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('DB is OK'))
+  .catch(() => console.log('DB failed'));
+  
+
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
