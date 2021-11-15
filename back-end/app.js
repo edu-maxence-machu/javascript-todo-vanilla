18d7b1d417c5e4604cd77fc4e15f9e61f5b97cff
Maxence Machu <maxence.machu@akarah.com>
2021-11-15 10:17:22 +0100

    exemple(chat)

--- back-end/app.js
+++ back-end/app.js
@@ -1,17 +1,16 @@
 require('dotenv').config();
 const express = require('express');
-const bodyParser = require('body-parser');
 const sDemo = require('./models/demo');
 
 // export one function that gets called once as the server is being initialized
 module.exports = function(app, server) {
 
-  const mongoose = require('mongoose');
-  mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URL}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
-    { useNewUrlParser: true,
-      useUnifiedTopology: true })
-    .then(() => console.log('DB is OK'))
-    .catch(() => console.log('DB failed'));
+const mongoose = require('mongoose');
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URL}/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('DB is OK'))
+  .catch(() => console.log('DB failed'));
     
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -30,6 +29,47 @@ const io = require('socket.io')(server, {
 })
 
 /*Nous pouvons utiliser la constante io pour le websocket*/
+io.on('connection', (socket) => {
+  socket.on('disconnect', () => {
+    console.log(`user ${socket.id} disconnected`);
+    io.emit('notification', `Bye ${socket.id}`);
+  });
+
+  console.log(`Connecté au client ${socket.id}`);
+  io.emit('notification', `Bonjour, ${socket.id}`);
+
+  socket.on('chat', (message) => {
+    console.log(`Jai recu : ${message.data}` );
+
+    io.emit('chat', message.data);
+  })
+/* 
+  socket.on('join', (prev, room) => {
+    
+    if(prev !== ''){
+      socket.leave(prev);
+    }    
+    socket.join(room);
+
+  }); */
+
+  /* socket.on('message', (msg) => {
+    // On envoie un message à la room sélectionnée
+    console.log('room:', msg.room);
+    console.log('message:', msg.message);
+    io.in(msg.room).emit('message', msg.message);
+  });
+
+  io.of("/").adapter.on("create-room", (room) => {
+    console.log(`room ${room} was created`);
+  });
+  
+  io.of("/").adapter.on("join-room", (room, id) => {
+    console.log(`socket ${id} has joined room ${room}`);
+  }); */
+})
+
+
 
 app.use(function(req, res, next) {req.io = io; next(); });
 
