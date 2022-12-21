3c883140f390da86d49e46c6000bb76972ae7c7a
Maxence Machu <maxence@akarah.com>
2022-12-21 10:54:47 +0100

    Feat: add chat exemple

--- back-end/socket/socket.js
+++ back-end/socket/socket.js
@@ -1,15 +1,35 @@
 module.exports = function(io){
-    // Établissement de la connexion à Socket.io
+
+io.use( async (socket, next) => {
+  let userId = socket.request._query['userId'];
+  let userSocketId = socket.id;      
+  next();    
+  
+  /* TODO: Ajouter UserSocket ID en base de données */
+  // EX UPDATE user SET socketid = ?, online= ? WHERE id = ?`;
+});
+
+let userList = []
+// Établissement de la connexion à Socket.io
 io.on('connection', (socket) =>{
     socket.on('disconnect', () => {
       console.log(`user ${socket.id} disconnected`);
       io.emit('notif', `Bye ${socket.id}`);
+
+      userList = userList.filter(user => user.id !== socket.id)
+      io.emit('exit', userList);
     });
   
-    socket.on('message', (msg) => {
-      console.log(msg);
-      io.emit('message', msg);
+    socket.on('message', ({text, from}) => {
+      io.emit('message', `${from} : ${text}`);
     });
+  
+    socket.on('private', ({text, to,from}) => {
+      io.emit(to, `${from} : text ${text}`);
+    });
+
+    userList.push({id: socket.id, name: socket.id})
+    io.emit('enter', userList);
   
     console.log(`Connecté au client ${socket.id}`)
     io.emit('notif', `Bonjour à toi ! ${socket.id}`);
